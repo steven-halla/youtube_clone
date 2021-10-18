84c5bd11d6a4a10b751dfa8c1d95723e5bc642bf
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-10-18 14:32:20 -0700

    "users can click and select videos "

--- src/components/SearchResultsListView.jsx
+++ src/components/SearchResultsListView.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import {buttonClasses} from "@mui/material";
 
 /*
  * this component accepts a searchResults object, which contains the list of "items" that youtube returned to us via api.
  * we also want to pass in handleSetVideo, so that we can set the a video to display/play when user clicks on a search result.
  */
 export const SearchResultsListView = (props) => {
-  const { searchResults, handleSetVideo } = props;
+  const {searchResults, handleSetVideo} = props;
 
   if (searchResults == null) {
     return <>search results is null</>;
@@ -15,49 +16,24 @@ export const SearchResultsListView = (props) => {
     return <>no match found</>;
   }
 
-  return(
-    <div>
-        {searchResults.items.map(item => {
-          // console.log(item);
-          // const videoId = item.id.videoId
-          // return <VideoPlayer videoId={videoId}/>
-          return <SearchResultItem item={item}/>
-          }
-        )}
-    </div>
-  )
+return(
+  <div>
+    {console.log("parent function:", props)}
+    {searchResults.items.map((item, index) => {
+      {console.log("item", item)}
+      return(
+        <button key={index} onClick={() => {props.handleSetVideo(item)}}>
+                <img src={item.snippet.thumbnails.default.url}
+                alt="searched image"/>
+        </button>
+      )
+    })}
+  </div>
+
+)
+
 }
 
 
-const SearchResultItem = (props) => {
-  const item = props.item;
-  // handleSetVideo = item;
-  console.log(item);
 
 
-  // const video = (props) => {
-  //   return <SearchResultItem item={item}/>
-  // }
-
-  console.log("Incoming Props: ", props);
-
-  return (
-    <div>
-      {/*<a href={item.id.videoId}>*/}
-
-      {/*  <img src={item.snippet.thumbnails.default.url} alt="" />*/}
-
-      {/*</a>*/}
-
-      <button onClick={() => {props.handleSetVideo(item)}}>
-        <img src= { item.snippet.thumbnails.default.url}
-             alt="searched image"
-             />
-      </button>
-
-      {/*{item.snippet.thumbnails.default.url}*/}
-      {/*{item.items.map()}*/}
-
-    </div>
-  );
-}
