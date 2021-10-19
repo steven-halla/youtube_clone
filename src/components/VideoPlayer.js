689a9773efad1409a1a4aaed828474a47fa405c4
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-10-19 15:32:25 -0700

    "added extra styling, fixed video sizing with em values, also increased size of description and title"

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,31 +1,41 @@
 import React, {useState} from 'react';
+import styled from "@emotion/styled";
 
-
-
+const CreateVideoPlayer = styled.div `
+  .video-title {
+    color: black;
+    font-size: 2.5em;
+  }
+  
+  .video-description {
+    color:black;
+    font-size: 1.8em;
+  }
+`
 
 export const VideoPlayer = (props) => {
   const {videoId, title, description} = props;
-  const {video, setVideo} = useState();
-
 
   if (!videoId) {
     return null;
   }
 
   return (
-    <div>
-      <iframe
-        id="ytplayer"
-        title={title}
-        width="640"
-        height="360"
-        src={`https://www.youtube.com/embed/${videoId}`}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        frameBorder="0"
-        allowFullScreen
-      />
-      <p className="video-title">Title:{title}</p>
-      <p>Description:{description}</p>
-    </div>
-  )
+    <CreateVideoPlayer>
+      <div>
+        <iframe
+          id="ytplayer"
+          title={title}
+          width="640"
+          height="360"
+          src={`https://www.youtube.com/embed/${videoId}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          frameBorder="0"
+          allowFullScreen
+        />
+        <p className="video-title">Title:{title}</p>
+        <p className="video-description" >Description:{description}</p>
+      </div>
+    </CreateVideoPlayer>)
+
 }
